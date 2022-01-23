953dce63933c405f8cc71f6821428b93e9ba4983
levischimelfining <levi.schimelfining@gmail.com>
2022-01-22 23:05:27 -0800

    Testing

--- lib/camera_control.py
+++ lib/camera_control.py
@@ -1,72 +1,209 @@
 import cv2
-frame = cv2.imread('/home/pi/DeepPiCar/driver/data/road1_240x320.png')
-hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import numpy as np
+import math
+
+_SHOW_IMAGE = False
 
 
-class Image:
+class HandCodedLaneFollower(object):
 
-    def detect_edges(frame):
-        lower_blue = np.array([60, 40, 40])
-        upper_blue = np.array([150, 255, 255])
-        mask = cv2.inRange(hsv, lower_blue, upper_blue)
-        # filter for blue lane lines
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        show_image("hsv", hsv)
-        lower_blue = np.array([60, 40, 40])
-        upper_blue = np.array([150, 255, 255])
-        mask = cv2.inRange(hsv, lower_blue, upper_blue)
-        show_image("blue mask", mask)
+    def __init__(self, car=None):
+        self.car = car
+        self.curr_steering_angle = 0
 
-        # detect edges
-        edges = cv2.Canny(mask, 200, 400)
+    def follow_lane(self, frame):
+        # Main entry point of the lane follower
+        show_image("orig", frame)
 
-        return edges
+        lane_lines, frame = detect_lane(frame)
+        final_frame = self.steer(frame, lane_lines)
 
-    def region_of_interest(edges):
-        height, width = edges.shape
-        mask = np.zeros_like(edges)
+        return final_frame
 
-        # only focus bottom half of the screen
-        polygon = np.array([[
-            (0, height * 1 / 2),
-            (width, height * 1 / 2),
-            (width, height),
-            (0, height),
-        ]], np.int32)
+    def steer(self, frame, lane_lines):
+        if len(lane_lines) == 0:
+            return frame
 
-        cv2.fillPoly(mask, polygon, 255)
-        cropped_edges = cv2.bitwise_and(edges, mask)
-        return cropped_edges
+        new_steering_angle = compute_steering_angle(frame, lane_lines)
+        self.curr_steering_angle = stabilize_steering_angle(self.curr_steering_angle, new_steering_angle, len(lane_lines))
 
-    def detect_line_segments(cropped_edges):
-        # tuning min_threshold, minLineLength, maxLineGap is a trial and error process by hand
-        rho = 1  # distance precision in pixel, i.e. 1 pixel
-        angle = np.pi / 180  # angular precision in radian, i.e. 1 degree
-        min_threshold = 10  # minimal of votes
-        line_segments = cv2.HoughLinesP(cropped_edges, rho, angle, min_threshold,
-                                        np.array([]), minLineLength=8, maxLineGap=4)
+        if self.car is not None:
+            self.car.front_wheels.turn(self.curr_steering_angle)
+        curr_heading_image = display_heading_line(frame, self.curr_steering_angle)
+        show_image("heading", curr_heading_image)
 
-        return line_segments
+        return curr_heading_image
 
-#init camera
-print("start color detect")
-camera = PiCamera()
-camera.resolution = (640,480)
-camera.framerate = 24
-rawCapture = PiRGBArray(camera, size=camera.resolution)
 
-for frame in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
-    img = frame.array
-    img,img_2,img_3 =  color_detect(img,'red')
-    cv2.imshow("video", img)
-    cv2.imshow("mask", img_2)
-    cv2.imshow("morphologyEx_img", img_3)
-    rawCapture.truncate(0)
+def detect_lane(frame):
 
-    k = cv2.waitKey(1) & 0xFF
-    if k == 27:
-        camera.close()
-        break
+    edges = detect_edges(frame)
+    show_image('edges', edges)
+
+    cropped_edges = region_of_interest(edges)
+    show_image('edges cropped', cropped_edges)
+
+    line_segments = detect_line_segments(cropped_edges)
+    line_segment_image = display_lines(frame, line_segments)
+    show_image("line segments", line_segment_image)
+
+    lane_lines = average_slope_intercept(frame, line_segments)
+    lane_lines_image = display_lines(frame, lane_lines)
+    show_image("lane lines", lane_lines_image)
+
+    return lane_lines, lane_lines_image
+
+
+def detect_edges(frame):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    show_image("hsv", hsv)
+    lower_blue = np.array([30, 40, 0])
+    upper_blue = np.array([150, 255, 255])
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    show_image("blue mask", mask)
+
+    # detect edges
+    edges = cv2.Canny(mask, 200, 400)
+
+    return edges
+
+
+def region_of_interest(edges):
+    height, width = edges.shape
+    mask = np.zeros_like(edges)
+
+    # only focus bottom half of the screen
+    polygon = np.array([[
+        (0, height * 1 / 2),
+        (width, height * 1 / 2),
+        (width, height),
+        (0, height),
+    ]], np.int32)
+
+    cv2.fillPoly(mask, polygon, 255)
+    cropped_edges = cv2.bitwise_and(edges, mask)
+    return cropped_edges
+
+
+def detect_line_segments(cropped_edges):
+    # tuning min_threshold, minLineLength, maxLineGap is a trial and error process by hand
+    rho = 1  # distance precision in pixel, i.e. 1 pixel
+    angle = np.pi / 180  # angular precision in radian, i.e. 1 degree
+    min_threshold = 10  # minimal of votes
+    line_segments = cv2.HoughLinesP(cropped_edges, rho, angle, min_threshold,
+                                    np.array([]), minLineLength=8, maxLineGap=4)
+
+    return line_segments
+
+
+def average_slope_intercept(frame, line_segments):
+    """
+    This function combines line segments into one or two lane lines
+    If all line slopes are < 0: then we only have detected left lane
+    If all line slopes are > 0: then we only have detected right lane
+    """
+    lane_lines = []
+    if line_segments is None:
+        return lane_lines
+
+    height, width, _ = frame.shape
+    left_fit = []
+    right_fit = []
+
+    boundary = 1/3
+    left_region_boundary = width * (1 - boundary)  # left lane line segment should be on left 2/3 of the screen
+    right_region_boundary = width * boundary # right lane line segment should be on left 2/3 of the screen
+
+    for line_segment in line_segments:
+        for x1, y1, x2, y2 in line_segment:
+            if x1 == x2:
+                continue
+            fit = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = fit[0]
+            intercept = fit[1]
+            if slope < 0:
+                if x1 < left_region_boundary and x2 < left_region_boundary:
+                    left_fit.append((slope, intercept))
+            else:
+                if x1 > right_region_boundary and x2 > right_region_boundary:
+                    right_fit.append((slope, intercept))
+
+    left_fit_average = np.average(left_fit, axis=0)
+    if len(left_fit) > 0:
+        lane_lines.append(make_points(frame, left_fit_average))
+
+    right_fit_average = np.average(right_fit, axis=0)
+    if len(right_fit) > 0:
+        lane_lines.append(make_points(frame, right_fit_average))
+
+    return lane_lines
+
+
+def compute_steering_angle(frame, lane_lines):
+    """ Find the steering angle based on lane line coordinate
+        We assume that camera is calibrated to point to dead center
+    """
+    if len(lane_lines) == 0:
+        return -90
+
+    height, width, _ = frame.shape
+    if len(lane_lines) == 1:
+        x1, _, x2, _ = lane_lines[0][0]
+        x_offset = x2 - x1
+    else:
+        _, _, left_x2, _ = lane_lines[0][0]
+        _, _, right_x2, _ = lane_lines[1][0]
+        camera_mid_offset_percent = 0.02 # 0.0 means car pointing to center, -0.03: car is centered to left, +0.03 means car pointing to right
+        mid = int(width / 2 * (1 + camera_mid_offset_percent))
+        x_offset = (left_x2 + right_x2) / 2 - mid
+
+    # find the steering angle, which is angle between navigation direction to end of center line
+    y_offset = int(height / 2)
+
+    angle_to_mid_radian = math.atan(x_offset / y_offset)  # angle (in radian) to center vertical line
+    angle_to_mid_deg = int(angle_to_mid_radian * 180.0 / math.pi)  # angle (in degrees) to center vertical line
+    steering_angle = angle_to_mid_deg  # this is the steering angle needed by picar front wheel
+
+    return steering_angle
+
+
+def stabilize_steering_angle(curr_steering_angle, new_steering_angle, num_of_lane_lines,
+                             max_angle_deviation_two_lines=5, max_angle_deviation_one_lane=1):
+    """
+    Using last steering angle to stabilize the steering angle
+    This can be improved to use last N angles, etc
+    if new angle is too different from current angle, only turn by max_angle_deviation degrees
+    """
+    if num_of_lane_lines == 2:
+        # if both lane lines detected, then we can deviate more
+        max_angle_deviation = max_angle_deviation_two_lines
+    else:
+        # if only one lane detected, don't deviate too much
+        max_angle_deviation = max_angle_deviation_one_lane
+
+    angle_deviation = new_steering_angle - curr_steering_angle
+    if abs(angle_deviation) > max_angle_deviation:
+        stabilized_steering_angle = int(curr_steering_angle
+                                        + max_angle_deviation * angle_deviation / abs(angle_deviation))
+    else:
+        stabilized_steering_angle = new_steering_angle
+
+    return stabilized_steering_angle
+
+
+# init camera
+if __name__ == "__main__":
+
+    print("start color detect")
+    camera = PiCamera()
+    camera.resolution = (640,480)
+    camera.framerate = 24
+    rawCapture = PiRGBArray(camera, size=camera.resolution)
+
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        follower = HandCodedLaneFollower()
+        follower.follow_lane(frame)
+        follower.steer(frame, detect_lane(frame))
+
