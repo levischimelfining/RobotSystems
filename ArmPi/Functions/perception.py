432483953c9434d2e4b6d6f76afe42c06ad1ab44
levischimelfining <levi.schimelfining@gmail.com>
2022-02-19 21:42:41 -0800

    Testing

---
+++ ArmPi/Functions/perception.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+# coding=utf8
+import sys
+sys.path.append('/home/levi/ArmPi/')
+import cv2
+import time
+import Camera
+import threading
+from LABConfig import *
+from ArmIK.Transform import *
+from ArmIK.ArmMoveIK import *
+import HiwonderSDK.Board as Board
+from CameraCalibration.CalibrationConfig import *
+
+
+class Perception:
+
+    def __init__(self, img):
+        self.img = img
+        self.rect = None
+        self.size = (640, 480)
+
+    def run(self):
+        img_copy = self.img.copy()
+        img_h, img_w = img.shape[:2]
+        cv2.line(img, (0, int(img_h / 2)), (img_w, int(img_h / 2)), (0, 0, 200), 1)
+        cv2.line(img, (int(img_w / 2), 0), (int(img_w / 2), img_h), (0, 0, 200), 1)
+
+        frame_resize = cv2.resize(img_copy, self.size, interpolation=cv2.INTER_NEAREST)
+        frame_gb = cv2.GaussianBlur(frame_resize, (11, 11), 11)
+        frame_lab = cv2.cvtColor(frame_gb, cv2.COLOR_BGR2LAB)  # Convert image to LAB space
+
+        area_max = 0
+        areaMaxContour = 0
+
+        for i in color_range:
+            if i in __target_color:
+                detect_color = i
+                frame_mask = cv2.inRange(frame_lab, color_range[detect_color][0], color_range[detect_color][
+                    1])  # Bitwise operations on the original image and mask
+                opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))  # open operation
+                closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))  # closed operation
+                contours = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]  # find the outline
+                areaMaxContour, area_max = getAreaMaxContour(contours)  # find the largest contour
+        if area_max > 2500:  # have found the largest area
+            rect = cv2.minAreaRect(areaMaxContour)
+            box = np.int0(cv2.boxPoints(rect))
+
+            roi = getROI(box)  # get roi region
+            get_roi = True
+
+            img_centerx, img_centery = getCenter(rect, roi, size,
+                                                 square_length)  # Get the coordinates of the center of the block
+            world_x, world_y = convertCoordinate(img_centerx, img_centery, size)  # Convert to real world coordinates
+
+            cv2.drawContours(img, [box], -1, range_rgb[detect_color], 2)
+            cv2.putText(img, '(' + str(world_x) + ',' + str(world_y) + ')', (min(box[0, 0], box[2, 0]), box[2, 1] - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, range_rgb[detect_color], 1)  # draw center point
+
+
+if __name__ == '__main__':
+    my_camera = Camera()
+    my_camera.camera_open()
+    while True:
+        img = my_camera.frame
+        if img is not None:
+            cv2.imshow('img', img)
+            key = cv2.waitKey(1)
+            if key == 27:
+                break
+    my_camera.camera_close()
+    cv2.destroyAllWindows()
